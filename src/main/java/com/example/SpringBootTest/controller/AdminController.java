02c579d84c21a82ae42183d248f087b67ad7d202
Dimantostibal <Dimantostibal@mail.ru>
2021-08-02 15:28:44 +0300

    Initial commit

--- src/main/java/com/example/SpringBootTest/controller/AdminController.java
+++ src/main/java/com/example/SpringBootTest/controller/AdminController.java
@@ -3,11 +3,9 @@ package com.example.SpringBootTest.controller;
 import com.example.SpringBootTest.model.User;
 import com.example.SpringBootTest.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
 import java.util.*;
 
 @RestController
